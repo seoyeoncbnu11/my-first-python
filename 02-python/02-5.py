8f861c8bad146d5fae5ad12805bac2198ec5c2a8
Yu Seoyeon <kksy103@naver.com>
2025-02-08 15:50:24 +0900

    Update Chapter 2

--- 02-python/02-5.py
+++ 02-python/02-5.py
@@ -1,8 +1,28 @@
-age = 13
-name = 'Elice'
-grade = ['A+', 'B+', 'A0']
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(3 / 5)
+num = 3
+print(num + 5)
 
-my_var = 1
-my_var = 'python'
-my_var = [1, 'a', 'python']
-print(my_var)
+num1 = 12
+num2 = num1 + 10
+answer = num2 / 2
+print('네가 생각한 답은', answer, '이야!')
+
+print(13 / 5)
+print(13 // 5)
+print(13 % 5)
+print(2 ** 10)
+
+ans1 = 7 % 3
+ans2 = 2 ** 4
+print(ans1, ans2)
+
+print('안녕' + '하세요')
+print('안녕' * 3)
+
+str1 = '덩덕' + '쿵덕'
+str2 = '쿵덕' * 2
+jajinmori = str1 + str2 + str1 + str2
+print(jajinmori)
