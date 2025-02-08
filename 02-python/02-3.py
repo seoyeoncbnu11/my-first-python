10091230e50a3171f5ca81e57972c86ace39ec6d
Yu Seoyeon <kksy103@naver.com>
2025-02-08 15:21:02 +0900

    Upload Chapter 2

---
+++ 02-python/02-3.py
@@ -0,0 +1,3 @@
+print(23)
+print('Yu Seo yeon')
+print([23, 'Yu seo yeon'])
---
+++ 02-python/02-5.py
@@ -0,0 +1,8 @@
+age = 13
+name = 'Elice'
+grade = ['A+', 'B+', 'A0']
+
+my_var = 1
+my_var = 'python'
+my_var = [1, 'a', 'python']
+print(my_var)
